9f203199b759de68d2ed63aa57aeec9f486494c8
Evan Mckenzie <evanmckenzie013@gmail.com>
2021-02-03 12:26:22 -0500

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,16 @@
+# test script for Game of Life with python
+# much of what is functional from this script will likely be applicable for Arduino
+
+# import numpy
+# import rand
+
+
+# Generate matrix of 1 and 0 (100x100)
+
+# initialize iterators i and j
+
+# while(true)
+#   draw grid with 0 as white square and 1 as black square
+#   [x_11 x_12 x_13;
+#    x_21 x_22 x_23;
+#    x_31 x_32 x_33]
