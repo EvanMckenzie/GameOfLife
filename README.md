297da3a6ff8ad053eacc8f84ee069fec8e7a8677
Evan Mckenzie <evanmckenzie013@gmail.com>
2021-02-09 16:43:39 -0500

    Update README.md, python_test.py
    fixed typo in README.md
    added logic to check neighbor cell status

--- README.md
+++ README.md
@@ -6,18 +6,16 @@ The Game of Life, also known as Life, was devised by John Horton Conway. It's a 
 
 # RULES:
 
-- Life is an infinite, 2D orthogonal grid of square "cells", each of which is in two possible states, live or dead. 
+- Life is an infinite, 2D orthogonal grid of square "cells", each of which is in two possible states, live or dead.
 
 - Every cell interacts with its eight neighbors that are horizontally, vertically, and diagonally adjacent.
 
-- At each step in time, the following transtions occur:
-  
+- At each step in time, the following transitions occur:
+
   - Any live cell with fewer than 2 live neighbors dies, as if by underpopulation
-  
+
   - Any live cell with 2 or 3 live neighbors lives on to the next generation
-  
+
   - Any live cell with greater than 3 live neighbors dies, as if by overpopulation
-  
+
   - Any dead with exactly 3 live neighbors becomes a live cell, as if by reproduction
-  
-  
--- python_test.py
+++ python_test.py
@@ -2,16 +2,35 @@
 # much of what is functional from this script will likely be applicable for Arduino
 
 # import numpy
-# import rand
-
+# import matplotlib
+import numpy as np
+import matplotlib as plt
 
 # Generate matrix of 1 and 0 (100x100)
+game_grid = np.random.randint(2, size=(10,10))
 
-# initialize iterators i and j
+# initialize num_row and num_col variables
+game_grid_size = game_grid.shape()
 
-# while(true)
-#   draw grid with 0 as white square and 1 as black square
-#   [x_11 x_12 x_13;
-#    x_21 x_22 x_23;
-#    x_31 x_32 x_33]
-# modulo or bitwise operator to check if you're at the end of array and move to other side
+num_row = game_grid_size[0]
+num_col = game_grid_size[1]
+
+# initialize count for neighboring cells
+neighbor_count = 0
+
+# for loops to index into matrix
+for i in range(num_row):
+    for j in range(num_col):
+        for row in range(i-1,i+1):
+            for col in range(j-1,j+1):
+                if (row == i) and (col == j):
+                    pass
+                neighbor_count += game_grid[row%num_row,col%num_col]
+                if game_grid[i,j] == 1:
+                    if neighbor_count == 2 or neighbor_count == 3:
+                        game_grid[i,j] = 1
+                    else:
+                        game_grid[i,j] = 0
+                elif game_grid[i,j] = 0:
+                    if neighbor_count == 3:
+                        game_grid[i,j] = 1
