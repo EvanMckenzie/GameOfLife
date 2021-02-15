98bcc9a0f03c0fa5381ec049cd6352f6007e48f0
Evan Mckenzie <evanmckenzie013@gmail.com>
2021-02-15 13:59:27 -0500

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -9,48 +9,41 @@ import matplotlib.animation as animation
 
 # declare global variables
 N = 10 # size of square grid
+ON = 1
+OFF = 0
+# make game grid
+VALS =[ON,OFF]
+GAME_GRID = np.random.choice(VALS,N*N,p=[0.2,0.8]).reshape(N,N)
 
-def check_neighbors(grid_row,grid_col):
+def check_neighbors(grid_row,grid_col,grid):
+    neighbor_count = 0
     for row in range(grid_row-1,grid_row+1):
         for col in range(grid_col-1,grid_col+1):
             if row == grid_row and col == grid_col:
                 continue
-            neighbor_count += game_grid[row%N,col%N]
+            neighbor_count += grid[row%N,col%N]
     return neighbor_count
 
-def set_cell_status():
+def set_cell_status(data):
+    global GAME_GRID
+    new_grid = GAME_GRID.copy()
+
     for i in range(N):
         for j in range(N):
-            neighbor_sum = check_neighbors(i,j)
+            neighbor_sum = check_neighbors(i,j,GAME_GRID)
 
-            if game_grid[i,j] == 1:
-                if neighbor_sum == 2 or neighbor_sum == 3:
-                    game_grid[i,j] = 1
-                else:
-                    game_grid[i,j] = 0
-            elif game_grid[i,j] == 0:
+            if GAME_GRID[i,j] == ON:
+                if (neighbor_sum < 2) or (neighbor_sum > 3):
+                    new_grid[i,j] = OFF
+            else:
                 if neighbor_sum == 3:
-                    game_grid[i,j] = 1
+                    new_grid[i,j] = ON
 
-def animate_grid(grid):
-    # ANIMATION USING MATPLOTLIB --TO BE UPDATED--
-    fig, ax = plt.subplots()
-    mat = ax.matshow(grid)
-    ani = animation.FuncAnimation(fig, update, interval=50,
-                                  save_count=50)
-    plt.show()
+    GAME_GRID = new_grid
+    mat.set_data(GAME_GRID)
+    return mat
 
-def main():
-    try:
-        # make game grid
-        game_grid=[]
-        game_grid = np.random.choice(game_grid,N*N,p=[0.2,0.8]).reshape(N,N)
-
-        #put into while loop to have run until user cancels it
-        while True:
-            # call set_cell_status to get into grid
-            set_cell_status()
-            # call animate_grid to make it run
-            animate_grid(game_grid)
-    except KeyboardInterrupt:
-        pass
+fig, ax = plt.subplots()
+mat = ax.matshow(GAME_GRID)
+ani = animation.FuncAnimation(fig, set_cell_status, interval=50, save_count=50, blit=True)
+plt.show()
