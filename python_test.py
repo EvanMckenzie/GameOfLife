6f2ea13613b308aa7baba1e4ee582b7d6d7ae46d
Evan Mckenzie <evanmckenzie013@gmail.com>
2021-02-17 16:02:31 -0500

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -8,12 +8,36 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # declare global variables
-N = 100 # size of square grid
+N = 10 # size of square grid
 ON = 1
 OFF = 0
+
+# define dictionary to reference RGB values for colors that will be assigned to cells
+COLOR_DICT = {
+    "red": [255,0,0],
+    "orange": [255,125,0],
+    "yellow": [255,255,0],
+    "sea_green": [125,255,0],
+    "green": [0,255,0],
+    "turquoise": [0,255,125],
+    "cyan": [0,255,255],
+    "ocean": [0,125,255],
+    "blue": [0,0,255],
+    "violet": [125,0,255],
+    "magenta": [255,0,255],
+    "raspberry": [255,0,125]
+}
+
 # make game grid
 VALS =[ON,OFF]
-GAME_GRID = np.random.choice(VALS,N*N,p=[0.2,0.8]).reshape(N,N)
+COLORS = ["red","orange","yellow","sea_green","green","turquoise","cyan","ocean","blue","violet","magenta","raspberry"]
+GAME_GRID = []
+for i in range(N):
+    for j in range(N):
+        GAME_GRID.append([int(np.random.choice(VALS,1,p=[0.2,0.8])),str(np.random.choice(COLORS,1))])
+#GAME_GRID = np.random.choice(VALS,N*N,p=[0.2,0.8]).reshape(N,N)
+print(GAME_GRID)
+
 
 def check_neighbors(grid_row,grid_col,grid):
     neighbor_count = (grid[grid_row, (grid_col-1)%N] + grid[grid_row, (grid_col+1)%N] +
